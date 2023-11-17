b0f5c5b0731a1ff4bcd39e7ea82e8057c090f1f4
Balaguru S <balagurusiva2002@gmail.com>
2023-11-17 18:56:52 +0530

    db connection, server creation and basic route file are created

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv"
+
+import userRoutes from './routes/users.js'
+import authUserRoutes from './routes/authUser.js'
+
+dotenv.config()
+const app = express()
+
+//middleware
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("common"))
+
+//routes
+app.use('/api/user', userRoutes)
+app.use('/api/auth', authUserRoutes)
+
+
+
+
+//DB connection and server creation
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => {
+        console.log("DB connected")
+        app.listen(process.env.PORT, console.log("listen at ", process.env.PORT))
+    })
+    .catch((error) => console.log(error))
