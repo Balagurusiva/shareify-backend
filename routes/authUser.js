ee57728e7f7b4a4103b5a19c1a28e56afcabba0f
Balaguru S <balagurusiva2002@gmail.com>
2023-11-17 19:27:44 +0530

    modified:   package-lock.json 	modified:   package.json 	user auth created :    routes/authUser.js

--- routes/authUser.js
+++ routes/authUser.js
@@ -1,9 +1,20 @@
 import express from 'express'
-
+import {User} from '../models/users.js'
 const router = express.Router()
 
-router.get('/',(req,res) =>{
-    return res.send("auth page")
+router.post  ('/', async (req,res) =>{
+     const newUser =  {
+        userName : req.body.userName,
+        email : req.body.email,
+        password : req.body.password
+     }
+
+     try {
+         const user = await User.create(newUser);
+         return res.status(200).send("user account created")
+     } catch (error) {
+        console.log(error)
+     }
 })
 
 export default router
