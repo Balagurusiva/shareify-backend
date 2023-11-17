fa71a42b30bcc3d249e95395b7898e5a3fbda0ff
Balaguru S <balagurusiva2002@gmail.com>
2023-11-17 19:06:39 +0530

    user model created

---
+++ models/users.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+
+const UserSchema = mongoose.Schema({
+    userName:{
+        type:String,
+        required:true,
+        min:3,
+        max:20,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true, 
+        unique:true
+    },
+    userName:{
+        type:String,
+        required:true,
+        min:8,
+        max:16
+    },
+    profilePicture:{
+        type:String,
+        default:""
+    },
+    coverPicture:{
+        type:String,
+        default:""
+    },
+    follower:{
+        type:Array,
+        default:[]
+    },
+    following:{
+        type:Array,
+        default:[]
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    }
+})
+
+ export const User = mongoose.model("User", UserSchema)
