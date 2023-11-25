8927a48390e95129f6dfdcd0180ed8f4924af7bb
Balaguru S <balagurusiva2002@gmail.com>
2023-11-25 11:56:50 +0530

    user model updated

--- models/users.js
+++ models/users.js
@@ -1,44 +1,60 @@
 import mongoose from 'mongoose'
 
 const UserSchema = mongoose.Schema({
-    userName:{
-        type:String,
-        required:true,
-        min:3,
-        max:20,
-        unique:true
+    userName: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 20,
+        unique: true
     },
-    email:{
-        type:String,
-        required:true, 
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true,
-        min:8,
-        max:16
+    password: {
+        type: String,
+        required: true,
+        min: 8,
+        max: 16
     },
-    profilePicture:{
-        type:String,
-        default:""
+    profilePicture: {
+        type: String,
+        default: ""
     },
-    coverPicture:{
-        type:String,
-        default:""
+    coverPicture: {
+        type: String,
+        default: ""
     },
-    follower:{
-        type:Array,
-        default:[]
+    follower: {
+        type: Array,
+        default: []
     },
-    following:{
-        type:Array,
-        default:[]
+    following: {
+        type: Array,
+        default: []
     },
-    isAdmin:{
-        type:Boolean,
-        default:false
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    desc: {
+        type: String,
+        max: 50
+    },
+    city: {
+        type: String,
+        max: 50
+    },
+    from: {
+        typr: String,
+        max: 50
+    },
+    relationShip: {
+        type: Number,
+        enum: [1, 2, 3]
     }
-})
+}, { timestamps: true })
 
- export const User = mongoose.model("User", UserSchema)
+export const User = mongoose.model("User", UserSchema)
